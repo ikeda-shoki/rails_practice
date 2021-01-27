af534085370f037f52a61642e48bcb5db162d7c5
ikeda-shoki <ikeda963552@gmail.com>
2021-01-27 16:02:41 +0000

    [Update]post-image,post-comment

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '0fce7565426447122559aa61b2377e2eeff134a80fab3354d9ca8f7f8edf1b8663ec63f491c57ea2bfa72b1abeda5de2c8dee09b29d06540281671102f47a79a'
